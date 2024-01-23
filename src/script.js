6bd1908fa14a1503883ea2afad4b6f7282958346
Hassan <hhalnajj@uci.edu>
2024-01-23 14:06:59 -0800

    Added images and links to icons

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('a').forEach(anchor => {
+document.querySelectorAll('.link-area a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
